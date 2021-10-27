6af5c7a9898109093953457d37a44807b8bdced7
zackosowski <zacharyo@mystma.org>
2021-10-27 17:30:45 -0500

    init

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMSparkMax;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+/** This is a demo program showing how to use Mecanum control with the RobotDrive class. */
+public class Robot extends TimedRobot {
+  private static final int kFrontLeftChannel = 3;
+  private static final int kRearLeftChannel = 2;
+  private static final int kFrontRightChannel = 1;
+  private static final int kRearRightChannel = 0;
+
+  private static final int kJoystickChannel = 0;
+
+  private MecanumDrive m_robotDrive;
+  private XboxController m_stick;
+
+  @Override
+  public void robotInit() {
+    WPI_TalonSRX frontLeft = new WPI_TalonSRX(kFrontLeftChannel);
+    WPI_TalonSRX rearLeft = new WPI_TalonSRX(kRearLeftChannel);
+    WPI_TalonSRX frontRight = new WPI_TalonSRX(kFrontRightChannel);
+    WPI_TalonSRX rearRight = new WPI_TalonSRX(kRearRightChannel);
+
+    // Invert the left side motors.
+    // You may need to change or remove this to match your robot.
+    frontLeft.setInverted(true);
+    rearLeft.setInverted(true);
+
+    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+
+    m_stick = new XboxController(kJoystickChannel);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    // Use the joystick X axis for lateral movement, Y axis for forward
+    // movement, and Z axis for rotation.
+    m_robotDrive.driveCartesian(m_stick.getY(Hand.kLeft), m_stick.getX(Hand.kLeft), m_stick.getX(Hand.kRight));
+  }
+}
